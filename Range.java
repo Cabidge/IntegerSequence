02f3a9433c081de4566629ba3bc329d26f4e012d
wdong20 <wdong20@stuy.edu>
2020-12-05 14:28:54 -0500

    Created the constructor

--- Range.java
+++ Range.java
@@ -5,7 +5,9 @@ public class Range implements IntegerSequence {
     //@param start : the starting value (inclusive) which must be <= end.
     //@param end : the ending value which is also inclusive.
     public Range(int start,  int end) { 
-
+        this.start = start;
+        this.end = end;
+        current = start;
     }
 
     public void reset() {
