b08f4606c53e50bcb68196f7689d7bef1ccde966
wdong20 <wdong20@stuy.edu>
2020-12-05 20:12:38 -0500

    Added test for length() method

--- Tester.java
+++ Tester.java
@@ -1,7 +1,7 @@
 public class Tester {
     public static void main(String[] zqfr) {
         IntegerSequence r = new Range(10,15);
-        System.out.println("Expected:\n10, 11, 12, 13, 14, 15");
+        System.out.println("Expected sequence:\n10, 11, 12, 13, 14, 15");
         while (r.hasNext()) {
             System.out.print(r.next());
             if (r.hasNext()) {       
@@ -9,5 +9,8 @@ public class Tester {
             }
         }
         System.out.println();
+
+        System.out.println("Expected length:\n6");
+        System.out.println(r.length());
     }
 }
