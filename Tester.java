9d54aa7f4cab83b5b6b84de09fe0afb943c97133
wdong20 <wdong20@stuy.edu>
2020-12-05 20:10:17 -0500

    Added test for simple sequence (totally not taken straight from #class-notes)

--- Tester.java
+++ Tester.java
@@ -1,5 +1,13 @@
 public class Tester {
     public static void main(String[] zqfr) {
-        
+        IntegerSequence r = new Range(10,15);
+        System.out.println("Expected:\n10, 11, 12, 13, 14, 15");
+        while (r.hasNext()) {
+            System.out.print(r.next());
+            if (r.hasNext()) {       
+                System.out.print( ", " );
+            }
+        }
+        System.out.println();
     }
 }
