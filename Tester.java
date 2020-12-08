7789403bcae5c5b6c8e2b73ecb964f80ff51d719
wdong20 <wdong20@stuy.edu>
2020-12-08 11:40:12 -0500

    Added simple test case for ArraySequence

--- Tester.java
+++ Tester.java
@@ -27,5 +27,15 @@ public class Tester {
         System.out.println("Expected value:\n10");
         r.reset();
         System.out.println(r.next());
+        System.out.println();
+
+        // ArraySequence
+        int[]nums = {1,3,5,0,-1,3,9};
+        IntegerSequence as = new ArraySequence(nums);
+
+        System.out.println("ArraySequence(array):");
+        while(as.hasNext()){
+            System.out.print(as.next()+", ");
+        }
     }
 }
